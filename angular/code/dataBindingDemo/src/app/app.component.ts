2721b08fa7a1db0163274fa84ef49eb49b2d053e
prasad-bigdp <bigdp585@gmail.com>
2025-05-01 18:00:07 +0530

    update

---
+++ angular/code/dataBindingDemo/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  isDisable = false;
+  count = 0;
+  n = 'PRASAD';
+  a = 10;
+  b = 20;
+  imgUrl:string =
+    'https://m.media-amazon.com/images/M/MV5BZGViNzZiMDEtZDk4Ny00MzNhLWI5ZTItZDY4MjAwMDk5OWQxXkEyXkFqcGc@._V1_.jpg';
+  names: string[] = [ "geetha", "seetha", "shiva" ]
+  user = {
+    name:"shekhar",age:55
+  }
+  incr() {
+    this.count++; 
+  }
+}
